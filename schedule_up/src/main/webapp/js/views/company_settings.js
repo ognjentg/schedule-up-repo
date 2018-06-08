fefdffe6778ff4184a7aedc3bfc3947b2808af07
DESKTOP-NJ5S71C\Maja <maja.mihajlovic9@gmail.com>
2018-06-09 00:32:17 +0200

    Popravljene dimenzije kalendara.

--- schedule_up/src/main/webapp/js/views/company_settings.js
+++ schedule_up/src/main/webapp/js/views/company_settings.js
@@ -18,76 +18,79 @@ var companySettingsView = {
             id: "customizeForm",
             elementsConfig: {
                 labelWidth: 290,
-                bottomPadding: 18
+                bottomPadding: 18,
+                    width:400
             },
 
-            elements: [{
-                id: "timeFrom",
-                name: "timeFrom",
-                view: "datepicker",
-                stringResult: true,
-                width:400,
-                label: "Početak radnog vremena:",
-                timepicker: true,
-                type: "time",
-                format: "%H:%i",
-                suggest: {
-                    type: "calendar",
-                    body: {
+            elements: [{ margin:5, cols:[
+
+                    {margin:5,rows:[ {
+                        id: "timeFrom",
+                        name: "timeFrom",
+                        view: "datepicker",
+                        stringResult: true,
+                        width:400,
+                        label: "Početak radnog vremena:",
+                        timepicker: true,
                         type: "time",
-                        calendarTime: "%H:%i"
-                    }
-                }
-            },
-                {
-                    id: "timeTo",
-                    width:400,
-                    name: "timeTo",
-                    view: "datepicker",
-                    stringResult: true,
-                    label: "Kraj radnog vremena:",
-                    timepicker: true,
-                    type: "time",
-                    format: "%H:%i",
-                    suggest: {
-                        type: "calendar",
-                        body: {
-                            type: "time",
-                            calendarTime: "%H:%i"
+                        format: "%H:%i",
+                        suggest: {
+                            type: "calendar",
+                            body: {
+                                type: "time",
+                                calendarTime: "%H:%i"
+                            }
                         }
-                    }
-                }, {
-            id:"reminder",
-                    view:"combo",
-                    width:400,
-                    value:"One",
-                    options:["0 minuta", "5 minuta", "15 minuta","30 minuta", "1 sat","12 sati","1 dan","1 sedmica"],
-                    label:"Podsjetnik:"
-                },{
-            view:"combo",
-            id:"cancellation",
-            label:"Minimalno vrijeme za otkazivanje sastanka:",
-                    width:400,
-            options:["15 minuta","30 minuta", "1 sat","12 sati","1 dan","1 sedmica"]
-                }]
-        },{type:"space", css:'white', cols:[
-            ]
-        },]
-    },
-    formatDate:function format(obj){
-    if (obj.value){
-        if (webix.isArray(obj.value))
-            return obj.value.map(webix.i18n.dateFormatStr).join("<br>");
-        return webix.i18n.dateFormatStr( obj.value );
-    }
-    return "";
-},
+                    },{
+                        id: "timeTo",
+                        width:400,
+                        name: "timeTo",
+                        view: "datepicker",
+                        stringResult: true,
+                        label: "Kraj radnog vremena:",
+                        timepicker: true,
+                        type: "time",
+                        format: "%H:%i",
+                        suggest: {
+                            type: "calendar",
+                            body: {
+                                type: "time",
+                                calendarTime: "%H:%i"
+                            }
+                        }
+                    }, {
+                            id:"reminder",
+                            view:"combo",
+                            width:400,
+                            value:"One",
+                            options:["0 minuta", "5 minuta", "15 minuta","30 minuta", "1 sat","12 sati","1 dan","1 sedmica"],
+                            label:"Podsjetnik:"
+                        },{
+                            view:"combo",
+                            id:"cancellation",
+                            label:"Minimalno vrijeme za otkazivanje sastanka:",
+                            width:400,
+                            options:["15 minuta","30 minuta", "1 sat","12 sati","1 dan","1 sedmica"]
+                        }
+                ]},{view:"calendar", id:"calendar"},{rows:[{id:"aa",view:"label",label:"Neradni dani:"},{
+                        id:"t3",template:function format(obj){
+                            if (obj.value){
+                                if (webix.isArray(obj.value))
+                                    return obj.value.map(webix.i18n.dateFormatStr).join("<br>");
+                                return webix.i18n.dateFormatStr( obj.value );
+                            }
+                            return "";
+                        }, height: 185
+                    }]}
+                    ]}
+                ]
+        }]},
     customizeDialog: function () {
         var form = $$("showDialogForm");
         webix.ui(webix.copy(companySettingsView.showDialogForm)).show();
 
     },
-    selectPanel: function(){
+    selectPanel: function() {
         $$("main").removeView(rightPanel);
         rightPanel = "settingsPanel";
 
@@ -96,5 +99,4 @@ var companySettingsView = {
         $$("main").addView(webix.copy(panelCopy));
 
     }
-
         };
