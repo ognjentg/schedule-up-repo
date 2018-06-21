1179b689bd12d42f583c7386dac46648e33c6004
Nenad <nesov97@gmail.com>
2018-06-21 17:10:52 +0200

    UserGroupController - CRUD metode

--- schedule_up/src/main/java/ba/telegroup/schedule_up/controller/UserGroupController.java
+++ schedule_up/src/main/java/ba/telegroup/schedule_up/controller/UserGroupController.java
@@ -1,17 +1,18 @@
 package ba.telegroup.schedule_up.controller;
 
 import ba.telegroup.schedule_up.common.exceptions.BadRequestException;
+import ba.telegroup.schedule_up.common.exceptions.ForbiddenException;
 import ba.telegroup.schedule_up.controller.genericController.GenericController;
 import ba.telegroup.schedule_up.model.UserGroup;
 import ba.telegroup.schedule_up.repository.UserGroupRepository;
 import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,7 +24,7 @@ public class UserGroupController extends GenericController<UserGroup, Integer> {
         super(repo);
     }
 
-    @RequestMapping(value = {"/"}, method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public @ResponseBody
     List<UserGroup> getByCompanyId() {
         return ((UserGroupRepository)repo).getAllByCompanyIdAndDeletedEquals(userBean.getUser().getCompanyId(), (byte) 0);
@@ -43,5 +44,21 @@ public class UserGroupController extends GenericController<UserGroup, Integer> {
         else {
             throw new BadRequestException("Bad request");
         }
+    }
+
+    @Override
+    @Transactional
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public @ResponseBody
+    String update(@PathVariable Integer id,@RequestBody  UserGroup object) throws BadRequestException, ForbiddenException {
+        return super.update(id, object);
+    }
+
+    @Override
+    @Transactional
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public @ResponseBody UserGroup insert(@RequestBody UserGroup object) throws BadRequestException, ForbiddenException {
+        return super.insert(object);
     }
 }
