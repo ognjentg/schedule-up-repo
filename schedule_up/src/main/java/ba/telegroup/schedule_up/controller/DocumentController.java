b9c4ff50f1d2562d0b46ed689f253eb0ae1f4154
Ognjen1994 <ognjen.risti@gmail.com>
2018-06-24 00:08:56 +0200

    Izbaceno kastovanje iz DocumentController-a

--- schedule_up/src/main/java/ba/telegroup/schedule_up/controller/DocumentController.java
+++ schedule_up/src/main/java/ba/telegroup/schedule_up/controller/DocumentController.java
@@ -3,6 +3,7 @@ package ba.telegroup.schedule_up.controller;
 import ba.telegroup.schedule_up.controller.genericController.GenericController;
 import ba.telegroup.schedule_up.model.Document;
 import ba.telegroup.schedule_up.repository.DocumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
@@ -16,34 +17,37 @@ import java.util.List;
 @Scope("request")
 public class DocumentController extends GenericController<Document, Integer> {
 
-    public DocumentController(JpaRepository<Document, Integer> repo) {
-        super(repo);
-    }
+    private DocumentRepository documentRepository;
 
+    @Autowired
+    public DocumentController(DocumentRepository repo) {
+        super(repo);
+        this.documentRepository = repo;
+    }
 
     @RequestMapping(value = "/getAllByIdIsAfter/{id}", method = RequestMethod.GET)
     public @ResponseBody
     List<Document> getAllByIdIsAfter(@PathVariable Integer id) {
-        return ((DocumentRepository) repo).getAllByIdIsAfter(id);
+        return documentRepository.getAllByIdIsAfter(id);
     }
 
     @RequestMapping(value = "/getAllByIdIsBefore/{id}", method = RequestMethod.GET)
     public @ResponseBody
     List<Document> getAllByIdIsBefore(@PathVariable Integer id) {
-        return ((DocumentRepository) repo).getAllByIdIsBefore(id);
+        return documentRepository.getAllByIdIsBefore(id);
     }
 
     @RequestMapping(value = "/getAllByNameContains/{name}", method = RequestMethod.GET)
     public @ResponseBody
     List getAllByNameContains(@PathVariable String name) {
-        List<Document> documents = ((DocumentRepository) repo).getAllByNameContains(name);
+        List<Document> documents = documentRepository.getAllByNameContains(name);
         return new ArrayList<>(documents);
     }
 
     @RequestMapping(value = "/getAllByMeetingId/{id}", method = RequestMethod.GET)
     public @ResponseBody
     List getAllByUserId(@PathVariable Integer id) {
-        List<Document> documents = ((DocumentRepository) repo).getAllByMeetingId(id);
+        List<Document> documents = documentRepository.getAllByMeetingId(id);
         return new ArrayList<>(documents);
     }
 }
