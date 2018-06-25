d698589c0165411688f64b72f1454790ea8ad17b
Marko Malinović <markom1995@gmail.com>
2018-06-25 16:26:31 +0200

    Popravljen UserGroupHasUserController.

--- schedule_up/src/main/java/ba/telegroup/schedule_up/controller/UserGroupHasUserController.java
+++ schedule_up/src/main/java/ba/telegroup/schedule_up/controller/UserGroupHasUserController.java
@@ -4,6 +4,7 @@ import ba.telegroup.schedule_up.common.exceptions.BadRequestException;
 import ba.telegroup.schedule_up.controller.genericController.GenericController;
 import ba.telegroup.schedule_up.model.UserGroupHasUser;
 import ba.telegroup.schedule_up.model.UserGroupHasUserPK;
+import ba.telegroup.schedule_up.repository.UserGroupHasUserRepository;
 import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
@@ -16,15 +17,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @Scope("request")
 public class UserGroupHasUserController extends GenericController<UserGroupHasUser, UserGroupHasUserPK> {
-    public UserGroupHasUserController(JpaRepository<UserGroupHasUser, UserGroupHasUserPK> repo) {
+
+    private final UserGroupHasUserRepository userGroupHasUserRepository;
+
+    public UserGroupHasUserController(UserGroupHasUserRepository repo) {
         super(repo);
+        this.userGroupHasUserRepository = repo;
     }
 
     @SuppressWarnings("SameReturnValue")
     @RequestMapping(value = {"/{groupId}/{userId}"}, method = RequestMethod.DELETE)
     public @ResponseBody
     String delete(@PathVariable Integer groupId, @PathVariable Integer userId) throws BadRequestException {
-
         UserGroupHasUserPK userGroupHasUserPK = new UserGroupHasUserPK();
         userGroupHasUserPK.setUserGroupId(groupId);
         userGroupHasUserPK.setUserId(userId);
@@ -35,7 +39,6 @@ public class UserGroupHasUserController extends GenericController<UserGroupHasUs
             return "Success";
         }
         throw new BadRequestException("Bad Request");
-
     }
 
 }
