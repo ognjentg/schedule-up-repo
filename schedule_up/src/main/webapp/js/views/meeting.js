f8a71d592190bae8cec708ee2dbdb25944e87a88
Maja Mihajlovic <maja.mihajlovic9@gmail.com>
2018-06-28 16:33:55 +0200

    Sitne izmjene.

--- schedule_up/src/main/webapp/js/views/meeting.js
+++ schedule_up/src/main/webapp/js/views/meeting.js
@@ -86,45 +86,8 @@ var meetingView = {
                         view:"list",  id:"mylist", type:"uploader",
                         autoheight:true, borderless:true
                     },
-                    { view: "label",
-                        id: "label",
-                        label: "Dodajte učesnike:"},
-
-                    { view: "text",
-                        id: "email",
-                        name:"email",
-                        label: "E-mail nezaposlenih u kompaniji:",
-                    },
-                    {view: "activeList",
-                        id:"mylist",
-                        container:"testA",
-                        width:650,
-                        autoheight:true,
-                        select:true,
-                        scroll:"x",
-                        data:[
-                            { id:1, title:"The Shawshank Redemption", year:1994, rank:1, markCheckbox:1},
-                            { id:2, title:"The Godfather", year:1972,  rank:2, markCheckbox:0},
-                            { id:3, title:"The Godfather: Part II", year:1974, rank:3},
-                            { id:4, title:"The Good, the Bad and the Ugly", year:1966, rank:4, markCheckbox:1},
-                            { id:5, title:"My Fair Lady", year:1964, rank:5, markCheckbox:1},
-                            { id:6, title:"12 Angry Men", year:1957, rank:6, markCheckbox:0}
-                        ],
-                        activeContent:{
-
-                            markCheckbox:{
-                                view:"checkbox",
-                                width:50,
-                                on:{ /*checkbox onChange handler*/
-                                    'onChange':function(newv, oldv){
-                                        var item_id = this.config.$masterId;
-                                        var state = this.getValue()?"Check ":"Uncheck ";
-                                        webix.message(state+item_id);
-                                    }
-                                }
-                            }
-                        }
-                        },
+                      
+                    
                     {
                         margin: 5,
                         cols: [{}, {
@@ -154,25 +117,6 @@ var meetingView = {
     selectPanel: function (room) {
         detachAllEvents();
 
-        scheduler.attachEvent("onContextMenu", function(event_id, native_event_object) {
-            if (event_id) {
-                var posx = 0;
-                var posy = 0;
-                if (native_event_object.pageX || native_event_object.pageY) {
-                    posx = native_event_object.pageX;
-                    posy = native_event_object.pageY;
-                } else if (native_event_object.clientX || native_event_object.clientY) {
-                    posx = native_event_object.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-                    posy = native_event_object.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-                }
-                menu.showContextMenu(posx, posy);
-                return false; // prevent default action and propagation
-            }
-            return true;
-        });
-        webix.protoUI({
-            name:"activeList"
-        },webix.ui.list,webix.ActiveContent);
         $$("main").removeView(rightPanel);
         meetingView.roomId=room;
         rightPanel = "meetingPanel";
@@ -185,6 +129,7 @@ var meetingView = {
             $$("startTime").setValue(date);
 
         });
+
         schedulerEvents.push(event);
         scheduler.config.xml_date = "%d-%m-%Y %H:%i";
         scheduler.config.readonly = true;
@@ -205,10 +150,9 @@ var meetingView = {
         var formatter=webix.Date.dateToStr("%d-%m-%Y %H:%i");
         var today=new Date();
         if(Date.parse(form.getValues().startTime)<Date.now()) {
-            util.messages.showErrorMessage("Nije moguće napraviti odabrati datum koji je prošao.");
+            util.messages.showErrorMessage("Nije moguće odabrati datum koji je prošao.");
             return;
         }
-        console.log(formatter(form.getValues().startTime));
         if (form.validate()) {
             var newMeeting = {
                 start_date:formatter(form.getValues().startTime),
