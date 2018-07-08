c59f2889903eb778de5e1c6b89b279a21655e0d5
Marko Malinović <markom1995@gmail.com>
2018-07-08 09:09:34 +0200

    Popravljen upit za metodu koja vraca korisnike koje nisu u nekoj grupi.

--- schedule_up/src/main/java/ba/telegroup/schedule_up/repository/repositoryCustom/repositoryImpl/UserRepositoryImpl.java
+++ schedule_up/src/main/java/ba/telegroup/schedule_up/repository/repositoryCustom/repositoryImpl/UserRepositoryImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class UserRepositoryImpl implements UserRepositoryCustom {
 
     private static final String SQL_GET_NOT_IN_GROUP_BY_COMPANY_ID = "SELECT DISTINCT u.id, u.email, u.username, u.password, u.pin, u.first_name, u.last_name, u.photo, u.active, u.deleted, u.deactivation_reason, u.token, u.token_time, u.company_id, u.role_id FROM user u LEFT JOIN user_group_has_user ughu ON u.id=ughu.user_id WHERE ughu.user_group_id IS NULL AND u.company_id=?";
-    private static final String SQL_GET_NOT_IN_GROUP_BY_COMPANY_ID_AND_GROUP_ID = "SELECT DISTINCT u.id, u.email, u.username, u.password, u.pin, u.first_name, u.last_name, u.photo, u.active, u.deleted, u.deactivation_reason, u.token, u.token_time, u.company_id, u.role_id FROM user u LEFT JOIN user_group_has_user ughu ON u.id=ughu.user_id WHERE (ughu.user_group_id IS NULL OR ughu.user_group_id!=?) AND u.company_id=?";
+    private static final String SQL_GET_NOT_IN_GROUP_BY_COMPANY_ID_AND_GROUP_ID = "SELECT DISTINCT u.id, u.email, u.username, u.password, u.pin, u.first_name, u.last_name, u.photo, u.active, u.deleted, u.deactivation_reason, u.token, u.token_time, u.company_id, u.role_id FROM user u LEFT JOIN user_group_has_user ughu ON u.id=ughu.user_id WHERE (ughu.user_group_id IS NULL OR u.id NOT IN (SELECT ughu2.user_id FROM user_group_has_user ughu2 WHERE ughu2.user_group_id=?)) AND u.company_id=?";
 
     @PersistenceContext
     private EntityManager entityManager;
