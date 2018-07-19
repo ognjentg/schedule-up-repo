23d1f4ed36850166e4184cfe3410d8c14476ddf2
Vladimir Ceko <ceko.vladimir@gmail.com>
2018-07-19 15:58:59 +0200

    Logger filter sa ponudjenim vrijednostima i izgled filtera sa opsegom

--- schedule_up/src/main/webapp/js/views/logger.js
+++ schedule_up/src/main/webapp/js/views/logger.js
@@ -10,6 +10,63 @@ var loggerView = {
                 view: "label",
                 width: 400,
                 template: "<span class='fa fa-history'></span> Logger korisničkih akcija"
+            },{},{
+                id: "datePickerFilter",
+                view:"daterangepicker",
+                name:"dateRangePicker",
+                label:"Od-Do:",
+                value:{start: new Date(2018,1,1), end: new Date()},
+                on:{
+                    onChange: function (id) {
+                        util.messages.showMessage("to be implemented  "+id);
+                    }
+                }
+            },{
+                id: "richSelectFilter",
+                view: "richselect",
+                label: "Prikaži:",
+                value: 1,
+                editable: false,
+                options: [
+                    {id: 1, value: "Sve"},
+                    {id: 2, value: "Prošla 24h"},
+                    {id: 3, value: "Prošla sedmica"}
+                ],
+                on: {
+                    onChange: function (id) {
+                        //My custom date filter for last number of days
+                        var customFilterForDate = function(days){
+                            var today = new Date();
+                            var startDate;
+                            var tempDay = today.getDate()-days;
+                            if(tempDay>0 && today.getMonth()>1)
+                                startDate= new Date(today.getFullYear(),today.getMonth(),tempDay);
+                            else if (tempDay<0 && today.getMonth()>1)
+                                startDate= new Date(today.getFullYear(),today.getMonth()-1,tempDay);
+                            else if (tempDay<0 && today.getMonth()==1)
+                                startDate = new Date(today.getFullYear()-1,12,tempDay);
+                            $$("loggerDT").filter(function (obj) {
+                                var dateOfObj= new Date(obj.created);
+                                if (dateOfObj >= startDate)
+                                    return true;
+                                return false;
+                            })
+                        }
+                        switch (id){
+                            case 1:
+                                util.messages.showMessage("i guess something one");
+                                $$("loggerDT").filterByAll();
+                                break;
+                            case 2:
+                                customFilterForDate(1);
+                                util.messages.showMessage("i guess something 2");
+                                break;
+                            case 3:
+                                customFilterForDate(7);
+                                break;
+                        }
+                    }
+                }
             }]
         }, {
             view: "datatable",
